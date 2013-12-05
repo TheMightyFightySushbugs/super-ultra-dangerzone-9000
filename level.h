4f14c98f0ddeb1f6c704de0b990d5d1bbeeb0cf0
jcosta9 <jpaulo.caraujo@gmail.com>
2013-12-04 23:00:04 -0600

    Added coments for the documentation

--- level.h
+++ level.h
@@ -12,11 +12,27 @@ class Level
         std::string *nextLevel_str; //<-- contains filename of next level
                                     //    (null if this is last level)
     public:
+        /**
+         * Constructor for Level
+         *
+         * @param File a text file that contains the instructions for the level
+         */
         Level(const char *file);
         //void addEvent(int sec, int numEnemy, EnemyShip &enemy);
         //void defineLength(int length);
+        /**
+         * Populates the enemies list
+         *
+         * @param enemies a list of type EnemyShip
+         */
         bool update(std::list<EnemyShip *> &enemies);
+        /**
+         * Returns the pointer to the NextLevel
+         */
         std::string* getNextLevel() {return nextLevel_str;}
+        /**
+         * Cleans the list of events
+         */
         void cleanUpResources();
 };
 
